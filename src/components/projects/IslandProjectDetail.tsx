3e28edcf61d526ce7c7b4102f68939ba5ba3f217
DODOS137 <dohyon18@naver.com>
2025-06-11 18:31:50 +0100

    Update IslandProjectDetail.tsx

--- src/components/projects/IslandProjectDetail.tsx
+++ src/components/projects/IslandProjectDetail.tsx
@@ -225,13 +225,13 @@ const IslandProjectDetail = () => {
 
             {/*Design Image 4*/} 
             <div className="w-full mb-20 md:mb-40">
-              <img src="/lovable-uploads/6a470e16-2864-4cce-a936-ae10a67d2d06.png" className="w-full h-auto mt-20 md:mt-40" />
+              <img src="/lovable-uploads/web1920-island_대지 10.png" className="w-full h-auto mt-20 md:mt-40" />
            </div> 
 
 
             {/*Design Image 5*/} 
             <div className="w-full mb-20 md:mb-40">
-              <img src="/lovable-uploads/6a470e16-2864-4cce-a936-ae10a67d2d06.png" className="w-full h-auto mt-20 md:mt-40" />
+              <img src="/lovable-uploads/web1920-island_대지 15.png" className="w-full h-auto mt-20 md:mt-40" />
            </div> 
 
             
