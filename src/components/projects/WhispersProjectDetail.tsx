0ef2c8248265accef6614587772e2df96d19236e
DODOS137 <dohyon18@naver.com>
2025-06-13 01:35:52 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -217,7 +217,7 @@ const WhispersProjectDetail = () => {
 
           {/*Product Design Image1*/}        
           <div className="w-full">
-            <img className="w-full h-full mb-20 md:mb-40" src="/lovable-uploads/a7366b42-6e66-4599-9349-15d25c8c0162.png" />
+            <img className="w-full h-full mb-20 md:mb-40" src="/lovable-uploads/web1920-whispers from the bottom24final_대지 24.png" />
           </div>   
 
          <div className="flex flex-col md:flex-row md:items-start md:space-x-16">
