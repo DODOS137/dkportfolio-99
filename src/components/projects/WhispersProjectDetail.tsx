afc9ffdc4c74cd3f89878cb24124dc120b76e557
DODOS137 <dohyon18@naver.com>
2025-09-12 14:16:09 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -494,7 +494,7 @@ const WhispersProjectDetail = () => {
       
       {/*Navigation Section*/}
       <div className="pb-40 md:pb-60 flex items-center justify-center">
-        <Link to="/project/project-5" className="inline-flex items-center gap-3 px-6 md:px-8 py-3 md:py-4 bg-black text-white border border-white hover:bg-white hover:text-black transition-colors duration-300 rounded-md text-base md:text-lg font-medium">
+        <Link to="/project/invisible-space-museum" className="inline-flex items-center gap-3 px-6 md:px-8 py-3 md:py-4 bg-black text-white border border-white hover:bg-white hover:text-black transition-colors duration-300 rounded-md text-base md:text-lg font-medium">
           <span>Next project</span>
           <ArrowRight className="w-4 md:w-5 h-4 md:h-5" />
         </Link>
