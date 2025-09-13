abf46e9e1b7d0d6545d633b3488f216f79c52102
DODOS137 <dohyon18@naver.com>
2025-09-13 23:36:05 +0100

    Update InvisibleProjectDetail.tsx

--- src/components/projects/InvisibleProjectDetail.tsx
+++ src/components/projects/InvisibleProjectDetail.tsx
@@ -574,7 +574,7 @@ const InvisibleProjectDetail = () => {
         {/* Navigation */}
         <div className="pb-40 md:pb-60 flex items-center justify-center">
           <Link
-            to="/project/learn"
+            to="/project/Thermal-Trace"
             className="inline-flex items-center gap-3 px-6 md:px-8 py-3 md:py-4 bg-black text-white border border-white hover:bg-white hover:text-black transition-colors duration-300 rounded-md text-base md:text-lg font-medium"
           >
             <span>Next project</span>
