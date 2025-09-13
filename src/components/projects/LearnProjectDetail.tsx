a563842506e0cb8a4cdf83e03e24ee062460f6fc
DODOS137 <dohyon18@naver.com>
2025-09-13 23:37:52 +0100

    Update LearnProjectDetail.tsx

--- src/components/projects/LearnProjectDetail.tsx
+++ src/components/projects/LearnProjectDetail.tsx
@@ -454,7 +454,7 @@ const LearnProjectDetail = () => {
         
           {/*Void*/}
           <div className="pb-40 md:pb-60 flex items-center justify-center">
-            <Link to="/project/project-3" className="inline-flex items-center gap-3 px-6 md:px-8 py-3 md:py-4 bg-black text-white border border-white hover:bg-white hover:text-black transition-colors duration-300 rounded-md text-base md:text-lg font-medium">
+            <Link to="/project/Seoul-Nature-history-Museum" className="inline-flex items-center gap-3 px-6 md:px-8 py-3 md:py-4 bg-black text-white border border-white hover:bg-white hover:text-black transition-colors duration-300 rounded-md text-base md:text-lg font-medium">
               <span>Next project</span>
               <ArrowRight className="w-4 md:w-5 h-4 md:h-5" />
             </Link>
