345dcde6797aedaa67c5e2061cac81af1397f9a7
DODOS137 <dohyon18@naver.com>
2025-09-15 09:48:45 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -388,7 +388,7 @@ const WhispersProjectDetail = () => {
           {/* Graphic design images Section */}
  
             {/*Development Image1*/}     
-          <div className="w-full mb-8">
+          <div className="w-full">
             <img className="w-full h-full" src="/lovable-uploads/a0b20d87-ef7c-4183-9209-6abb798b0f65.png" />
           </div>           
           
@@ -417,12 +417,12 @@ const WhispersProjectDetail = () => {
 
           {/*Graphic Design Image1-1*/}        
           <div className="w-full">
-            <img className="w-full h-full mb-10 md:mb-10" src="/lovable-uploads/web1920-whispers from the bottom-27.png" />
+            <img className="w-full h-full mb-20 md:mb-20" src="/lovable-uploads/web1920-whispers from the bottom-27.png" />
           </div>   
 
           {/*Graphic Design Image1-2*/}        
           <div className="w-full">
-            <img className="w-full h-full mb-10 md:mb-10" src="/lovable-uploads/web1920-whispers from the bottom-28.png" />
+            <img className="w-full h-full mb-20 md:mb-20" src="/lovable-uploads/web1920-whispers from the bottom-28.png" />
           </div>   
 
            {/*Graphic Design Image1-3*/}        
