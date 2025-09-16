6a4f36da1d7941cd2df7c245d7e7e6e4698e2f1f
DODOS137 <dohyon18@naver.com>
2025-09-16 20:50:20 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -448,14 +448,16 @@ const WhispersProjectDetail = () => {
             <img className="w-full h-full mb-8 md:mb-8" src="/lovable-uploads/web1920-whispers from the bottom_대지 10 사본.png" />
           </div>
 
-
-          {/*Line*/} 
-          <div className="w-full h-px my-20 md:my-40 bg-gray-500/50"></div>  
-
           {/*Bridge Image1*/}        
           <div className="w-full">
             <img className="w-full h-full mb-10 md:mb-10" src="/lovable-uploads/33_1.png" />
           </div>
+
+
+          {/*Line*/} 
+          <div className="w-full h-px my-20 md:my-40 bg-gray-500/50"></div>  
+
+
 
           
 
