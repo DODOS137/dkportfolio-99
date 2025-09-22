223d9827c0795577fa50a3e7a0e7787058708214
DODOS137 <dohyon18@naver.com>
2025-09-22 17:28:47 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -624,14 +624,18 @@ const WhispersProjectDetail = () => {
 </div>
 
 
+          {/*Product Design Text*/} 
           <div className="flex flex-col md:flex-row md:items-start md:space-x-16">
-          <div className="rounded-lg bg-transparent flex flex_col md:flex-row md:items-start md:space-x-16">
+          <div className="rounded-lg bg-transparent flex flex-col md:flex-row md:items-start md:space-x-16">
           <h2 className="text-sm md:text-sm font-Medium text-gray-300 mb-3 min-w-[200px]">Product Design</h2>
-          <p className="text-sm md:text-sm lg:text-sm font-light text-gray-400">Custom-designed headset stands emulate smoothed underwater rocks, integrating both audio hardware and tactile visuality. Each plinth invites solitary listening through high-resolution recordings of marine species. The subtlety of these soundscapes becomes a form of protest against the visual-centric bias of most exhibitions.</p>
-          </div>
-          </div>
-
-          {/*Line*/} 
+          <p className="text-sm md:text-sm lg:text-sm font-light text-gray-400">Custom-designed headset stands emulate smoothed underwater rocks, integrating both audio hardware and tactile visuality. Each plinth invites solitary listening through high-resolution recordings of marine species. The subtlety of these soundscapes becomes a form of protest against the visual-centric bias of most exhibitions.
+          </p>
+         </div>
+         </div>
+          
+          
+          
+         {/*Line*/} 
           <div className="w-full h-px my-10 md:my-10 bg-transparent"></div>
 
           {/*Process Rendering Image1*/}        
