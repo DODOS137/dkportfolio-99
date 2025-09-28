ea4fc2264380d04eb5cad9350b7264d896431868
DODOS137 <dohyon18@naver.com>
2025-09-28 18:26:22 +0100

    Update SeoulMuseumProjectDetail.tsx

--- src/components/projects/SeoulMuseumProjectDetail.tsx
+++ src/components/projects/SeoulMuseumProjectDetail.tsx
@@ -596,28 +596,31 @@ Moved away from passive, linear layouts towards participatory and immersive expe
               </h2>
              <div className="flex flex-col space-y-4 md:space-y-4 ">
             <div>
-              <h3 className="text-sm md:text-sm lg:text-sm font-light mb-6 md:mb-8 min-w-[200px] text-gray-300">
+              <h3 className="text-sm md:text-sm lg:text-sm font-light mb-3 md:mb-3 min-w-[200px] text-gray-300">
                Overcrowded Specimen Displays
                </h3>
-              <p className="text-sm md:text-sm lg:text-sm leading-relaxed text-gray-400 font-light">The museum's densely classified displays obscure environmental context and hinder engagement, presenting specimens in ways misaligned with local sensibilities.</p>
+              <p className="text-sm md:text-sm lg:text-sm leading-relaxed text-gray-400 font-light mb-4 md:mb-4">The museum's densely classified displays obscure environmental context and hinder engagement, presenting specimens in ways misaligned with local sensibilities.</p>
 
-              <h3 className="text-sm md:text-sm lg:text-sm font-light mb-6 md:mb-8 min-w-[200px] text-gray-300">
+              <h3 className="text-sm md:text-sm lg:text-sm font-light mb-3 md:mb-3 min-w-[200px] text-gray-300">
                Passive Viewing Format
                </h3>
-              <p className="text-sm md:text-sm lg:text-sm leading-relaxed text-gray-400 font-light">The exhibition's linear layout limits engagement, reducing the experience to passive viewing with little interaction or depth.
+              <p className="text-sm md:text-sm lg:text-sm leading-relaxed text-gray-400 font-light mb-4 md:mb-4">The exhibition's linear layout limits engagement, reducing the experience to passive viewing with little interaction or depth.
               </p>
 
-              <h3 className="text-sm md:text-sm lg:text-sm font-light mb-6 md:mb-8 min-w-[200px] text-gray-300">
+              <h3 className="text-sm md:text-sm lg:text-sm font-light mb-3 md:mb-3 min-w-[200px] text-gray-300">
                Lack of Identity in Spatial Elements
                </h3>
               <p className="text-sm md:text-sm lg:text-sm leading-relaxed text-gray-400 font-light">The space lacks visual cohesion, with outdated elements creating a bland, institutional feel that weakens cultural resonance.
               </p>
               </div>
              </div>
-          
-  
             </div>
           </div>
+
+
+          {/*Line*/} 
+          <div className="w-full h-px my-10 md:my-10 bg-black"></div>
+          
 
           {/* Narrative */}
           <div className="w-full">
