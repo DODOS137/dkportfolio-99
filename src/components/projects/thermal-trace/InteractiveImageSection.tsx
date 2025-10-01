f78040551aef49093cf61ab767b1b350115b96c6
DODOS137 <dohyon18@naver.com>
2025-10-01 22:18:30 +0100

    Update InteractiveImageSection.tsx

--- src/components/projects/thermal-trace/InteractiveImageSection.tsx
+++ src/components/projects/thermal-trace/InteractiveImageSection.tsx
@@ -10,13 +10,13 @@ interface InteractiveImageSectionProps {
 
 const InteractiveImageSection = ({ baseImage, overlayImage, instruction = "Hover over the image and move your mouse from left to right." }: InteractiveImageSectionProps) => {
   return (
-    <div className="w-full h-auto mb-40">
+    <div className="w-full h-auto mb-20">
       <ImageSliceInteraction baseImage={baseImage} overlayImage={overlayImage} />
       <div className="flex flex-col md:flex-row md:items-start md:space-x-16 mt-8">
-        <h2 className="text-2xl font-light text-white whitespace-nowrap min-w-8 md:text-sm">
+        <h2 className="text-2xl font-Bold text-white whitespace-nowrap min-w-8 md:text-sm">
           →
         </h2>
-        <p className="text-sm leading-relaxed text-gray-700 font-light md:text-sm">{instruction}</p>
+        <p className="text-sm leading-relaxed text-white font-Medium md:text-sm">{instruction}</p>
       </div>
     </div>
   );
