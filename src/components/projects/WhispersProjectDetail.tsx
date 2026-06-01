486c821312b01642f55bb45832e0e5be41f57f50
DODOS137 <dohyon18@naver.com>
2026-06-01 13:43:05 +0100

    Add 'Project Video' heading to WhispersProjectDetail
    Add a title for the project video section.

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -323,6 +323,10 @@ const WhispersProjectDetail = () => {
     </AspectRatio>
   </div>
 
+  <p className="text-xl md:text-xl font-Medium text-black mb-6 relative z-10">
+    Project Video
+  </p>
+
   <AspectRatio ratio={16 / 9} className="relative z-10 rounded-lg border border-gray-800/50 overflow-hidden">
     <LiteYouTube
       id="zqz3Owz0K3o"
