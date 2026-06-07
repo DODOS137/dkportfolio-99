f9c02c42117d141ab7f90515d89766f885f32a7a
DODOS137 <dohyon18@naver.com>
2026-06-07 02:27:28 +0100

    Update SeoulMuseumProjectDetail.tsx

--- src/components/projects/SeoulMuseumProjectDetail.tsx
+++ src/components/projects/SeoulMuseumProjectDetail.tsx
@@ -572,7 +572,7 @@ The findings reveal a strong demand not only for architectural redefinition but 
        
 <div className="flex gap-5 items-start">
   {/* Image */}
-  <div className="w-1/2 h-[400px] overflow-hidden">
+  <div className="w-1/2 h-[10px] overflow-hidden">
     <img
       src="/webimages/SNM/SNM1.jpg"
       alt="Seoul Natural History Museum"
