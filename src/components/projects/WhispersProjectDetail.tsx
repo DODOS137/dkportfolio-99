da1c807f91450ee65a919fdab8c3555e6bc91f84
DODOS137 <dohyon18@naver.com>
2026-06-30 21:59:18 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -464,7 +464,7 @@ const WhispersProjectDetail = () => {
 
             {/* Poster Colour Variation Slider */}
             <div className="w-full flex justify-start">
-              <div className="inline-block">
+              <div className="w-[45%]">
                 <img
                   src={posterSlides[currentPosterSlide].image}
                   alt={posterSlides[currentPosterSlide].alt}
