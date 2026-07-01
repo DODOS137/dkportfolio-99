92e59eeecabfefcfe024b75b4d6ebbc81351670b
DODOS137 <dohyon18@naver.com>
2026-07-01 12:55:08 +0100

    Update WhispersProjectDetail.tsx

--- src/components/projects/WhispersProjectDetail.tsx
+++ src/components/projects/WhispersProjectDetail.tsx
@@ -527,7 +527,7 @@ const WhispersProjectDetail = () => {
 
             {/* GIF Section */}
             {/* GIF Board */}
-            <div className="w-full bg-[#907A56] p-[60px]">
+            <div className="w-full bg-[#3C2F21] p-[60px]">
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <div className="w-full aspect-[9/16] overflow-hidden">
                   <img
